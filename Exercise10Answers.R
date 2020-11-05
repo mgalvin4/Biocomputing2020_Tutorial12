dfe65e41fea5d11fb4936be1cc553f5eef2a9733
Maddie Galvin <mgalvin4@nd.edu>
2020-11-05 15:06:43 -0500

    Q2 Part 1

--- Exercise10Answers.R
+++ Exercise10Answers.R
@@ -1,5 +1,7 @@
 # Tutorial 10
 
+#Question 1
+
 library(ggplot2)
 setwd("/Users/Maddie Galvin/Desktop/Biocomputing2020_Tutorial12")
 dogs <- read.csv("DogWeight.csv", header=TRUE, stringsAsFactors=FALSE)
@@ -11,3 +13,17 @@ ggplot(data=dogs, aes(x=ï..Weeks, y=Weight)) +
   xlab("Weeks") +
   stat_smooth(method="lm") +
   theme_classic()
+
+# Question 2
+
+newdata = read.table("data.txt", header=TRUE, sep=",", stringsAsFactors=FALSE)
+dim(newdata)
+head(newdata)
+
+# Bar plot of average observations per region
+ggplot(data=newdata, aes(x=region, y=observations, fill=region)) +
+  stat_summary(fun = mean,
+               geom = "bar") +
+  ylab("average observations") +
+  theme_classic()
+
