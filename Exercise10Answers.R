5e25321ce9667ef85761b2059508a3a61e24dbe2
Maddie Galvin <mgalvin4@nd.edu>
2020-11-05 14:41:51 -0500

    update

--- Exercise10Answers.R
+++ Exercise10Answers.R
@@ -1,1 +1,12 @@
 # Tutorial 10
+
+library(ggplot2)
+setwd("/Users/Maddie Galvin/Desktop/Biocomputing2020_Tutorial12")
+dogs <- read.csv("DogWeight.csv", header=TRUE, stringsAsFactors=FALSE)
+head(dogs)
+tail(dogs)
+
+ggplot(data=dogs, aes(x=Weeks, y=Weight)) +
+  geom_point() +
+  stat_smooth(method="lm") +
+  theme_classic()
